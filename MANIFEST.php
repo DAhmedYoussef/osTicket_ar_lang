198ee7d7efa1b5035f717992dd1ef095368c9a73
Ahmed Youssef <158051044+DAhmedYoussef@users.noreply.github.com>
2024-01-29 14:30:38 +0200

    Add files via upload

---
+++ MANIFEST.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Build-Date' => 'Thu, 07 Sep 23 15:47:07 +0000',
+  'Phrases-Version' => '1.18',
+  'Build-Version' => 'v1.18',
+  'Build-Major-Version' => '1.18',
+  'Language' => 'ar_SA',
+  'Id' => 'lang:ar',
+  'Last-Revision' => '2023-07-11 20:55',
+  'Version' => 168910,
+);
